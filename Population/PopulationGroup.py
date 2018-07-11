674ff05ac671613a056c01d7963b7bcb4f7bb3ad
Klaus Mostegel <mostegel@MBI.ads>
2018-07-11 15:57:58 +0200

    Add atributes to Population Group

--- Population/PopulationGroup.py
+++ Population/PopulationGroup.py
@@ -1,1 +1,10 @@
+class PopulationGroup():
+
+    def __init__(self, gender,agegroup, employment, carAviable, averageIncome, paramsChoice):
+        self.gender=gender
+        self.agegroup=agegroup
+        self.employment=employment
+        self.carAviable=carAviable
+        self.averageIncome=averageIncome
+        self.paramsChoice=paramsChoice
 
