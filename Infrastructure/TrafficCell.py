367514c36a8661277c2f0a9ba74d9d976eac4ea6
Klaus Mostegel <mostegel@MBI.ads>
2018-07-12 16:12:16 +0200

    first attributes for PopulationGroup and TrafficCell

--- Infrastructure/TrafficCell.py
+++ Infrastructure/TrafficCell.py
@@ -1,1 +1,11 @@
+class TrafficCell():
+
+    def __init__(self, name):
+        self._name=name
+        self.populationParams=None
+        
+
+    def SetPopulationParams(self,populationParams):
+        self.populationParams=populationParams
+
 
--- Population/PopulationGroup.py
+++ Population/PopulationGroup.py
@@ -1,10 +1,20 @@
 class PopulationGroup():
 
-    def __init__(self, gender,agegroup, employment, carAviable, averageIncome, paramsChoice):
-        self.gender=gender
-        self.agegroup=agegroup
-        self.employment=employment
-        self.carAviable=carAviable
-        self.averageIncome=averageIncome
-        self.paramsChoice=paramsChoice
-
+    def __init__(self, name, gender,agegroup, employment, carAviable, averageIncome, paramsChoice):
+        self._name=name
+        self.__gender=gender #male or female
+        self.__agegroup=agegroup 
+        self.__employment=employment #yes or no 
+        self.__carAviable=carAviable
+        self.__averageIncome=averageIncome
+        self.__paramsChoice=paramsChoice
+        self._timebudget = None
+        
+    
+    
+    @staticmethod
+    def calculatePopulation(populationParams):
+        pass
+    
+    
+        
